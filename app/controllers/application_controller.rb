c1bf664fd96bcb3d2e531da4e83d3f9f16a97313
micahabanschick <micha.ban@gmail.com>
2020-08-27 20:40:04 -0400

    Initial

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+require_relative '../../config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+  end
+end
