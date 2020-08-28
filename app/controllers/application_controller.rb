841a3f1a4c67d2255d5209a0d7b690605110d182
micahabanschick <micha.ban@gmail.com>
2020-08-27 22:30:10 -0400

    /login=>Form=>/home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,29 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+    end
 
-  get '/' do
-  end
+    get '/' do 
+        # erb :index
+        username ||= nil 
+        password ||= nil 
+        if username == nil && password == nil 
+            redirect '/login'
+        else
+            redirect '/home'
+        end 
+    end
+
+    get '/login' do
+        erb :login
+    end
+
+    post '/home' do 
+        p = params
+        erb :home 
+    end 
+
 end
