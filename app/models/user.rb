9175917c6e08e0e092a42367957fbd397d5c0224
micahabanschick <micha.ban@gmail.com>
2020-09-24 12:26:15 -0400

    relevant models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,2 @@
-class User 
-
-    attr_accessor :username, :password
-    @@all = []
-
-    def initialize(args)
-        @username = args['username']
-        @password = args[:password]
-        self.save 
-    end
-
-    def save
-        @@all << self
-    end
-
-    def self.all 
-        @@all 
-    end
-    
-    def self.clear 
-        @@all = []
-    end
-
+class User < ActiveRecord::Base
 end 
