69b5d859162788e92a3056d1c9c48e669c74f423
micahabanschick <micha.ban@gmail.com>
2020-09-24 12:38:26 -0400

    basic database tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
     configure do
         set :public_folder, 'public'
         set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "password_security"
     end
 
     get '/' do 
@@ -29,23 +31,5 @@ class ApplicationController < Sinatra::Base
         # @username = params["user"][:username]
         erb :home 
     end 
-
-    helpers do
-        
-        def redirect_if_not_logged_in
-          if !logged_in?
-            redirect "/login?error=You have to be logged in to do that"
-          end
-        end
-    
-        def logged_in?
-          !!session[:user_id]
-        end
-    
-        def current_user
-          User.find(session[:user_id])
-        end
-    
-    end
 
 end
---
+++ db/migrate/04_create_comments.rb
@@ -0,0 +1,8 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+    def change 
+      create_table :comments do |t|
+        t.string :name
+        t.integer :user_id
+      end
+    end
+end
